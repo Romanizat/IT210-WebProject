d04e1cc2142f63eaec891791d74ab2610dc78c09
Romanizat <Romanizat@users.noreply.github.com>
2021-05-30 12:03:13 +0200

    Update Suggestions finished

---
+++ updateS.php
@@ -0,0 +1,24 @@
+<?php
+    require_once("connect.php");
+    session_start();
+    $id = $_POST["predlog"];
+    $mesto = $_POST["mesto"];
+    $zasto = $_POST["zasto"];
+    $username = $_SESSION["username"];
+    if(!isset($_SESSION['username'])){
+		header("Location: index.php");
+	}
+?>
+
+<?php
+    if(($id!=0) && ($mesto !="") && ($zasto !="")){
+        $stmt = $con->prepare("UPDATE locationsuggest SET mesto=:mesto, zasto=:zasto WHERE id=:id");
+        $stmt->bindParam(':mesto', $mesto);
+        $stmt->bindParam(':zasto',$zasto);
+        $stmt->bindParam(':id',$id);
+        $stmt->execute();
+        header('Location: profil.php');
+    }else {
+        header('Location: updateRes.php');
+    }
+?>
---
+++ updateSug.php
@@ -0,0 +1,106 @@
+<?php
+require_once("connect.php");
+session_start();
+$username=$_SESSION['username'];
+if (empty($_SESSION['username'])) {
+    header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="resources/favicon.png" />
+    <title>Umami | Predlog izmena</title>
+</head>
+
+<body>
+    <audio id="myAudio" loop>
+        <source src="resources/Hot Since 82 - Mr. Drive.mp3" type="audio/mp3">
+        Your browser does not support the audio element.
+    </audio>
+    <nav>
+        <h1 onclick="playAudio()" ondblclick="pauseAudio()">Umami 🌴</h1>
+        <ul class="nav-links">
+            <li><a href="index.php">Početna</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="umetnici.php">Umetnici</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+        </ul>
+    </nav>
+
+    <video autoplay muted loop id="bgVideo" src="resources/Pexels Videos 2759484.mp4" type="video/mp4">
+    </video>
+
+    <?php
+    if (!empty($_SESSION['username'])) {
+        $ime = $_SESSION['ime'];
+        $string = "
+      <div id=\"mySidenavB\" class=\"sidenav\">
+          <p>$ime</p>
+          <hr>
+          <a class=\"sideA\" href=\"rezervacijaPg.php\"><img src=\"resources/12-120181_png-file-ticket-icon-svg.png\" class=\"svg\" alt=\"tickets\"></a>
+          <hr>
+          <a class=\"sideA\" href=\"profil.php\"><img src=\"resources/person-icon-1682.png\" class=\"svg\" alt=\"person\"></a>
+          <hr>
+          <a class=\"sideA\" href=\"location.php\"><img src=\"resources/Simpleicons_Places_map-marker-1.svg\" class=\"svg\" alt=\"person\"></a>";
+
+        $admin = $_SESSION['admin'];
+        if ($admin == 1) {
+            $string = $string . "<hr>
+          <a class=\"sideA\" href=\"adminPanel.php\">Admin Panel</a>";
+        }
+        $string = $string . "   
+      <hr>
+      <a class=\"sideA\" href=\"logout.php\">Logout</a>
+    </div>";
+        echo $string;
+    }
+    ?>
+    <hr>
+
+    <?php
+        $sql = "SELECT * FROM korisnik WHERE username='".$username."'";
+        $result = $con->query($sql);
+        $row = $result->fetch();
+        $idKorisnik = $row["id"];
+
+        $sql2 = "SELECT * FROM locationsuggest l WHERE l.idKorisnik='".$idKorisnik."'";
+        $query = $con->query($sql2);
+        $results = array();
+        while ($results[] = $query->fetch());
+        array_pop($results);
+    ?>
+
+    <div class="centriranje">
+        <form method="POST" action="updateS.php" style="text-align: center;">
+            <h2>Koji predlog biste izmenili?</h2>
+            <select name="predlog" required>
+                <option value="">Izaberite predlog</option>
+                <?php foreach ($results as $option) { ?>
+                    <option value="<?php echo $option["id"]; ?>">
+                        <?php echo $option["mesto"]; ?>
+                    </option>
+                <?php } ?>
+            </select>
+            <br><br>
+            <h2>Šta biste izmenili?</h2>
+            <label for="msg">Unesite mesto i državu</label><br><br>
+            <textarea style="resize: none;" name="mesto" cols="50" rows="5" required></textarea><br><br>
+            <label for="zasto">Zašto?</label><br><br>
+            <textarea style="resize: none;" name="zasto" cols="50" rows="5" required></textarea><br><br>
+            <button type="submit" name="updateS">Izmeni</button>
+            <br> <br>
+        </form>
+    </div>
+    <footer>
+        <p class="fade-in3">© Marko Josifović 4494</p>
+    </footer>
+    <script src=script.js></script>
+</body>
+
+</html>
